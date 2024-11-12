957fd452ce9737357334f44454289d8263ae5e41
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2024-11-12 11:55:09 +0330

    feat: python platform

--- main.py
+++ main.py
@@ -1,47 +1,19 @@
+import http.server
+import socketserver
 import os
-import json
-import requests
-import time
 
+PORT = 8000
+DIRECTORY = "."
 
-def fetch_website_content(url):
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            return response.text
-        else:
-            return None
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return None
+class CustomHandler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
 
-def main():
-    try:
-        with open("websites.json", "r", encoding='utf-8') as file:
-            websites = json.load(file)
+    def do_GET(self):
+        if self.path == "/":
+            self.path = "/index.html"
+        return super().do_GET()
 
-        html_files_directory = "html_files"
-        if not os.path.exists(html_files_directory):
-            os.makedirs(html_files_directory)
-
-        while True:
-            for website in websites:
-                website_url = website["url"]
-                website_name = website["name"]
-                content = fetch_website_content(website_url)
-                if content:
-                    filename = f"{website_name}_{int(time.time())}.html"
-                    file_path = os.path.join(html_files_directory, filename)
-                    with open(file_path, "w", encoding='utf-8') as file:
-                        file.write(content)
-                        print(f"Content of {website_name} has been saved to {file_path}")
-                else:
-                    print(f"Failed to fetch content of {website_name}")
-
-            time.sleep(10)  
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-if __name__ == "__main__":
-    main()
+with socketserver.TCPServer(("", PORT), CustomHandler) as httpd:
+    print(f"Serving on port {PORT}")
+    httpd.serve_forever()
