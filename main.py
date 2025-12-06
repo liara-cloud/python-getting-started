08f49970ba4074e8ec2b5baa2961112699eeeadc
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2025-12-06 13:22:01 +0330

    add bcc and cc

--- main.py
+++ main.py
@@ -4,13 +4,9 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 # # uncomment in development mode: 
-# from dotenv import load_dotenv # pip install python-dotenv
-
-# # Load environment variables from .env file
+# from dotenv import load_dotenv # pip install dotenv
 # load_dotenv()
 
-
-# Read email configuration from environment variables
 MAIL_HOST = os.getenv('MAIL_HOST')
 MAIL_PORT = int(os.getenv('MAIL_PORT', 587))
 MAIL_USER = os.getenv('MAIL_USER')
@@ -18,28 +14,27 @@ MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
 MAIL_FROM_ADDRESS = os.getenv('MAIL_FROM_ADDRESS')
 
 def send_email(to_address, subject, body):
-    # Create the email message
     msg = MIMEMultipart()
     msg['From'] = MAIL_FROM_ADDRESS
     msg['To'] = to_address
     msg['Subject'] = subject
+    # msg['CC'] = 'test.one@example.com, test.two@example.com'
+    # msg['BCC'] = 'test.three@example.com,'
     msg.attach(MIMEText(body, 'plain'))
 
     try:
-        # Connect to the SMTP server
         server = smtplib.SMTP(MAIL_HOST, MAIL_PORT)
-        server.starttls()  # Secure the connection with TLS
-        server.login(MAIL_USER, MAIL_PASSWORD)  # Login to the SMTP server
-        server.send_message(msg)  # Send the email
+        server.starttls()  
+        server.login(MAIL_USER, MAIL_PASSWORD)  
+        server.send_message(msg)  
         print("Email sent successfully")
     except Exception as e:
         print("Failed to send email:", e)
     finally:
-        server.quit()  # Close the connection to the server
+        server.quit() 
 
-# Send an email to the specified recipient
-to_address = 'recipient@example.com'
-subject = 'Test Email'
-body = 'This is a test email sent from Python using environment variables.'
+to_address = 'test@example.com'
+subject    = 'Test Email'
+body       = 'This is a test email sent from Python using environment variables.'
 
 send_email(to_address, subject, body)
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Python Email Server
+
+```
+git clone https://github.com/liara-cloud/python-getting-started.git
+```
+```
+cd python-getting-started
+```
+```
+git checkout emailServer
+```
+```
+mv .env.example .env # and set ENVs
+```
+```
+python main.py
+```
